330bf8217623d27cfb1bebbe5ab154c377e86ca6
EerantiPrashanth <prashanth.e@aifalabs.com>
2024-08-06 11:30:10 +0530

    second

--- src/Components/UpperPart/UpperPart.jsx
+++ src/Components/UpperPart/UpperPart.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
 function UpperPart() {
-  return (
-    <div>UpperPart</div>
+  return (<>
+  <div>UpperPart</div>
+  <p>this is for prashanth</p>
+  </>
+    
+  
   )
 }
 
