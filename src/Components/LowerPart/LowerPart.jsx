32e3b91bee47a61f26383e49c0dc1c0c1930eb48
kalyan-kashaboina <kalyan.k@aifalabs.com>
2024-08-06 11:15:45 +0530

    Initial setup

---
+++ src/Components/LowerPart/LowerPart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LowerPart() {
+  return (
+    <div>LowerPart</div>
+  )
+}
+
+export default LowerPart
---
+++ src/Components/UpperPart/UpperPart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function UpperPart() {
+  return (
+    <div>UpperPart</div>
+  )
+}
+
+export default UpperPart
